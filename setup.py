e7ec8b1bc4ec007d0f7ac0a119e2bfb395d2268e
ERP Admin <erpadmin@ibtaxira.ibt.local>
2019-02-15 11:29:06 +0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 import re, ast
+
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
 
 # get version from __version__ variable in ibtcustom/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
@@ -10,17 +12,14 @@ with open('ibtcustom/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
-requirements = parse_requirements("requirements.txt", session="")
-
 setup(
 	name='ibtcustom',
 	version=version,
-	description='Customizations for IBT',
-	author='Finbyz Tech Pvt Ltd',
+	description='App to manage serverside customizations',
+	author='FinByz Tech Pvt Ltd',
 	author_email='info@finbyz.com',
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	install_requires=install_requires
 )
