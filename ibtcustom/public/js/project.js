c8337e279df3416f7268b2104a784d45093d629e
finbyz <info@finbyz.com>
2020-03-19 10:14:22 +0000

    project filteration

--- ibtcustom/public/js/project.js
+++ ibtcustom/public/js/project.js
@@ -1,12 +1,10 @@
-// cur_frm.fields_dict.project_type.get_query = function(doc) {
-// 	return {
-// 		filters: {
-//             "filters": [
-//                 ["project_type", "in", ["IT Hardware/Software","IT Infrastructure","Cloud Computing","Axira Business Solutions","IT Outsourcing – Part Time","IT Outsourcing – Full Time","One Time Support","Call Center Outsourcing","Business Process Outsourcing"]]
-//           ]
-// 	}
-// };
-// };
+cur_frm.fields_dict.project_type.get_query = function(doc) {
+	return {
+        "filters": [
+            ["project_type", "in", ["IT Hardware/Software","IT Infrastructure","Cloud Computing","Axira Business Solutions","IT Outsourcing – Part Time","IT Outsourcing – Full Time","One Time Support","Call Center Outsourcing","Business Process Outsourcing"]]
+        ]
+    };
+};
 
 frappe.ui.form.on("Project Task", {
     before_project_tasks_remove: function (frm, cdt, cdn) {
