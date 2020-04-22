4c15fd898e8aff55f439e920a936239b5279e061
finbyz <info@finbyz.com>
2020-04-22 10:26:18 +0000

    minor changes axira.py

--- ibtcustom/axira.py
+++ ibtcustom/axira.py
@@ -2,7 +2,7 @@
 from erpnext.projects.doctype.project.project import Project
 import frappe
 from frappe import _, sendmail
-from frappe.utils import today
+from frappe.utils import today,add_days
 
 def override_update_project(self):
 	Project.update_percent_complete(self)
