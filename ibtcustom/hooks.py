ee024299bb7b07e0f63e4ed6dc3d42129ddf073a
FinByz Tech Pvt. Ltd <info@finbyz.com>
2020-09-10 17:06:13 +0530

    Update hooks.py

--- ibtcustom/hooks.py
+++ ibtcustom/hooks.py
@@ -204,7 +204,7 @@ scheduler_events = {
 	"daily": [
 		"ibtcustom.api.send_employee_birthday_mails",
 		"ibtcustom.api.employee_anniversary_mails",
-		"ibtcustom.api.send_task_report_mail",
+		#"ibtcustom.api.send_task_report_mail",
 		"ibtcustom.api.make_todo",
 		"ibtcustom.api.update_issue_status",
 		"ibtcustom.api.disable_customer",
