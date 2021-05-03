3924b2834a8af194c48555767198842ebafc89c2
finbyz <info@finbyz.com>
2021-05-03 14:11:45 +0530

    changes

--- ibtcustom/hooks.py
+++ ibtcustom/hooks.py
@@ -28,7 +28,7 @@ app_include_css = "/assets/ibtcustom/css/ibtcustom.min.css"
 #app_include_js = "/assets/ibtcustom/js/ibtcustom.min.js"
 
 app_include_js = [
-	"assets/js/ibt.min.js" 
+	"/assets/js/ibt.min.js" 
 	#"assets/finbyzerp/js/frappe/ui/page.js"
 ]
 # include js, css files in header of web template
